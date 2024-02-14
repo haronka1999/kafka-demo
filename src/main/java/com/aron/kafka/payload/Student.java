4f0b358353f8833349e76b46482403e2b2939937
Aron Horvath <aron.horvath@delti.com>
2024-02-14 13:04:18 +0200

    Initial commit

---
+++ src/main/java/com/aron/kafka/payload/Student.java
@@ -0,0 +1,13 @@
+package com.aron.kafka.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+}
---
+++ src/main/java/com/aron/kafka/producer/KafkaJsonProducer.java
@@ -0,0 +1,7 @@
+package com.aron.kafka.producer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaJsonProducer {
+}
